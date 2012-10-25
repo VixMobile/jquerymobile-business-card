b064a674aa8bbde1ff5ea4f1e6b3c750ed5644b3
Carlos Justiniano <carlos.justiniano@gmail.com>
2012-10-25 02:08:46 -0400

    attempt to implement page scrolling when raw contacts collabsible is expanded and collapsed

--- js/app.js
+++ js/app.js
@@ -3,7 +3,15 @@ var appobj = {};
     var appName = 'jQueryMobile Business Card App';
     var version = '0.1';
     app.init = function() {
-        
+        $('#contactinfo').bind('expand', function() {
+            $(document).scrollTop($(document).height());  
+        }).bind('collapse', function() {
+            $(document).scrollTop(0);
+        });
     };
     app.init();
 })(appobj);
+
+$(document).ready(function() {
+    appobj.init();
+});
