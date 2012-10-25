45ab36926dfc614107a5b85b64b538970d076dbb
Carlos Justiniano <carlos.justiniano@gmail.com>
2012-10-25 00:31:03 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var appobj = {};
+(function(app) {
+    var appName = 'jQueryMobile Business Card App';
+    var version = '0.1';
+    app.init = function() {
+        
+    };
+    app.init();
+})(appobj);
